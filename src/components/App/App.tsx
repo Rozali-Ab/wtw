68785053c52f88f36abac712300c728fcb2137d8
Rozali-Ab <rozali_ab@outlook.com>
2023-08-22 21:44:46 +0300

    fix: rename App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import MainPage from '../../pages/MainPage/MainPage';
+
+import { TFilmCardInfo } from '../../pages/MainPage/MainPage';
+
+type TAppProps = {
+  film: TFilmCardInfo;
+}
+
+
+function App(props: TAppProps) {
+  const { film } = props;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: 
+        <MainPage filmCardInfo={film}/>
+    }
+  ]);
+  
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
