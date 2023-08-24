66b15c21c8779df8722efd24be962c58a781b0c5
Rozali-Ab <rozali_ab@outlook.com>
2023-08-24 22:49:08 +0300

    chores: add private routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,7 +10,8 @@ import MyListPage from '../../pages/MyListPage/MyListPage';
 import ErrorPage from '../../pages/ErrorPage/ErrorPage';
 
 import { TFilmCardInfo } from '../../pages/MainPage/MainPage';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthStatus } from '../../const';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 type AppProps = {
   film: TFilmCardInfo;
@@ -24,13 +25,31 @@ function App(props: AppProps) {
     <BrowserRouter>
       <Routes>
         <Route path={AppRoute.Root} element={ <Layout/>}>
-          <Route index element={ <MainPage filmCardInfo={film}/>}></Route>
-          <Route path={AppRoute.Film} element={ <FilmPage />}></Route>
-          <Route path={AppRoute.AddReview} element={ <AddReviewPage />}></Route>
-          <Route path={AppRoute.MyList} element={ <MyListPage />}></Route>
-          <Route path={AppRoute.Login} element={ <SignInPage />}></Route>
-          <Route path={AppRoute.Player} element={ <PlayerPage />}></Route>
-          <Route path="*" element={ <ErrorPage />}></Route>
+          <Route index element={ <MainPage filmCardInfo={film}/>} />
+          <Route path={AppRoute.Film} element={ <FilmPage />} />
+          <Route path={AppRoute.AddReview} 
+            element={ 
+              <PrivateRoute authStatus={AuthStatus.NoAuth}>
+                <AddReviewPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.MyList} 
+            element={ 
+              <PrivateRoute authStatus={AuthStatus.NoAuth}>
+                <MyListPage />
+              </PrivateRoute>
+            } 
+          />
+          <Route path={AppRoute.Login} element={ <SignInPage />} />
+          <Route path={AppRoute.Player} 
+            element={
+              <PrivateRoute authStatus={AuthStatus.NoAuth}>
+                <PlayerPage />
+              </PrivateRoute>
+            } 
+          />
+          <Route path="*" element={ <ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
