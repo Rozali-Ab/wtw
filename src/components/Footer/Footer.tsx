616c8fa2742b50854f6f9fad117b686736a107c6
Rozali-Ab <rozali_ab@outlook.com>
2023-08-26 18:48:25 +0300

    chores: update Logo component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,9 @@
+import Logo from '../Logo/Logo';
+
 function Footer() {
   return (
     <footer className="page-footer">
-      <div className="logo">
-        <a 
-          className="logo__link logo__link--light"
-          href="https://reactjs.org"
-        >
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
-      </div>
+      <Logo isLight/>
 
       <div className="copyright">
         <p>© 2019 What to watch Ltd.</p>
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 
-function Logo() {
+import { AppRoute } from '../../const';
+
+type LogoProps = {
+  isLight?: boolean;
+}
+
+function Logo( {isLight}: LogoProps) {
   return (
     <div className="logo">
-      <Link to="/" className="logo__link">
+      <Link to={AppRoute.Root} className={`logo__link ${isLight ? 'logo__link--light' : ''}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
