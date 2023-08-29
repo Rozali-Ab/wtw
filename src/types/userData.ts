9517e060cb7a4fc0d7a9ae0eed132c81e2c9e748
Rozali-Ab <rozali_ab@outlook.com>
2023-08-29 23:00:59 +0300

    add type userData

---
+++ src/types/userData.ts
@@ -0,0 +1,12 @@
+export type UserData = {
+  id: number;
+  name: string;
+  email: string;
+  avatarUrl: string;
+  token: string;
+};
+
+export type AuthData = {
+  email: string;
+  password: string;
+};
