f28b0415cbf2f71bbf7e2e5a78b8b8199e044cb9
Rozali-Ab <rozali_ab@outlook.com>
2023-09-08 15:50:59 +0300

    add ErrorMessage component

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+function ErrorMessage() {
+  return (
+    <div className="user-page">
+      <div className="catalog">
+        <h1>Oops</h1>
+        <p>Data is not loaded.<br/>Try again</p>
+      </div>
+    </div>
+    
+  );
+}
+
+export default ErrorMessage;
